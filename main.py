0a303e0284612c3f30b401b39e8a1f61e79f6732
Vilma <vilrox.28@gmail.com>
2022-05-11 12:49:03 -0500

    Se agregó el primer input

--- main.py
+++ main.py
@@ -1,8 +1,9 @@
 #1. Importar la librería Path de Python
 from pathlib import Path 
 
-print("""Bienvenid@ al CLI de archivos en el Sistema.
-Este es el directorio en el cual te encuentras:
+print("""
+Bienvenid@ al CLI de archivos en el Sistema.
+Este es el directorio en el cual te encuentras: 
 """)
 #2. Obtener la ruta del directorio actual   
 print(Path.cwd())
@@ -13,10 +14,11 @@ path = Path(directorio_actual)#Crear un objeto de tipo Path
 
 #4. Mostrarle al usuario las opciones a elegir
 print("""
-
-Elige una opción:
+Menú de opciones:
 a) Listar documentos.
 b) Leer un documento.
 c) Editar un documento.
 d) Salir.
 """)
+#5. Preguntar al usuario la tarea que desea realizar
+listar = print(input("¿Qué tarea deseas realizar? "))
